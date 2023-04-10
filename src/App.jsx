1f6dfe298ab2f31d393561c9593b022c08240ff3
PritrirajPartho <dapartho5616@gmail.com>
2023-04-10 10:06:29 +0600

    first-added

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+              <h1>Hi Assignment-nine</h1>
+              <h1>How are you?</h1>
+    </div>
+  )
+}
+
+export default App
